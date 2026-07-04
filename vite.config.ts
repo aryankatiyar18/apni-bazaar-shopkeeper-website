a72dbf2e9b8661bdaf337b04ce6f4786347ea947
aryankatiyar18 <katiyararyan4321@gmail.com>
2026-07-04 12:50:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "./", // This prevents the white screen on GitHub Pages!
-  plugins: [react(), tailwindcss()],
+  base: "./",
+  plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
